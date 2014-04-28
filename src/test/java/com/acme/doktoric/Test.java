23635f477ef355125d7665b4b861b1e5fc0a6f45
doktoric <doktoricsrichard@gmail.com>
2014-04-28 11:29:10 +0200

    modifications

---
+++ src/test/java/com/acme/doktoric/Test.java
@@ -0,0 +1,11 @@
+package com.acme.doktoric;
+
+import junit.framework.Assert;
+
+public class Test {
+
+    @org.junit.Test
+    public void test () {
+        Assert.assertEquals(true, false);
+    }
+}
