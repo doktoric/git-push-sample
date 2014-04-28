446c0bf980471f97dce42846c5e98418e8328e2a
doktoric <doktoricsrichard@gmail.com>
2014-04-28 10:41:55 +0200

    added starter

---
+++ src/main/java/com/acme/doktoric/Starter.java
@@ -0,0 +1,11 @@
+package com.acme.doktoric;
+
+/**
+ * Created by ricsi on 4/28/14.
+ */
+public class Starter {
+
+    public static void main (String args[]) {
+        System.out.println("hy");
+    }
+}
